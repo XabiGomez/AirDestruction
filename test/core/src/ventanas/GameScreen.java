d856b317c31784f94127e57b546b04d648e7654b
inigolens <81768110+inigolens@users.noreply.github.com>
2021-11-07 20:49:52 +0100

    Implementación de gestión de teclado y movimiento del jugador
    Implementación de gestión de teclado y movimiento del jugador con detección de bordes de pantalla. Fallo a la hora de calcular el borde derecho después de cambiar el tamaño de la pantalla.

--- test/core/src/ventanas/GameScreen.java
+++ test/core/src/ventanas/GameScreen.java
@@ -9,6 +9,9 @@ import com.mygdx.game.AirDestructionGame;
 public class GameScreen extends AbstractScreen {
 	private SpriteBatch batch;
 	private Texture texture;
+	private Texture jug;
+	private Jugador player;
+	int posyjugador = 100;
 	
 	public GameScreen(AirDestructionGame main) {
 		super(main);
@@ -17,6 +20,8 @@ public class GameScreen extends AbstractScreen {
 	public void show() {
 		batch = new SpriteBatch();
 		texture = new Texture("GameFondo.jpg");
+		jug = new Texture("snorlax.png");
+		player = new Jugador(calcularmitadpantX(),posyjugador,100,100,0,5);
 	}
 	
 	@Override
@@ -26,7 +31,49 @@ public class GameScreen extends AbstractScreen {
 
         batch.begin(); 
         batch.draw(texture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); 
+	batch.draw(jug, player.getX(),posyjugador,player.getAnchura(),player.getAltura());
         batch.end(); 
+	entradadatos();
     }
+	
+    public void entradadatos() {
+		boolean izquierdapulsada = Gdx.input.isKeyPressed(Input.Keys.A)||Gdx.input.isKeyPressed(Input.Keys.LEFT);
+		boolean derechapulsada = Gdx.input.isKeyPressed(Input.Keys.D)||Gdx.input.isKeyPressed(Input.Keys.RIGHT);
+		boolean intentadisparar = Gdx.input.isKeyPressed(Input.Keys.W)||Gdx.input.isKeyPressed(Input.Keys.UP)||Gdx.input.isKeyPressed(Input.Keys.SPACE);
+		if(izquierdapulsada!=derechapulsada) {
+			int nuevaX;
+			if(izquierdapulsada) {
+				nuevaX=(int) (player.getX()-player.getVelocidad());
+				if(tocaborde(nuevaX,player)) {
+					player.setX(0);
+				}else {
+					player.setX(nuevaX);
+				}
+			}else if(derechapulsada) {
+				nuevaX=(int) (player.getX()+player.getVelocidad());
+				if(tocaborde(nuevaX,player)) {
+					player.setX(Gdx.graphics.getWidth()-player.getAnchura());
+				}else {
+					player.setX(nuevaX);
+				}
+			}
+		}
+		if(intentadisparar) {
+			System.out.println("dispara");
+		}
+		
+	}
+	public int calcularmitadpantX() {
+		return  (int) Gdx.graphics.getWidth()/2;
+	}
+	public boolean tocaborde(int nuevaX,Jugador player) {
+		if(Gdx.graphics.getWidth()<(nuevaX+player.getAnchura())) {
+			return true;
+		}else if(nuevaX<0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 
 }
