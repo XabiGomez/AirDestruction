7f3e367ae0a9639ecfc3ed4fd75c97add70afefb
inigolens <81768110+inigolens@users.noreply.github.com>
2021-11-08 16:27:22 +0100

    Update Gamescreem disparar
    El bicho dispara y los disparos se eliminan después de salir de pantalla

--- test/core/src/ventanas/GameScreen.java
+++ test/core/src/ventanas/GameScreen.java
@@ -1,5 +1,7 @@
 package ventanas;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
@@ -7,7 +9,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.AirDestructionGame;
 
-import entidades.Jugador;
+import entidades.*;
 
 public class GameScreen extends AbstractScreen {
 	private SpriteBatch batch;
@@ -15,6 +17,10 @@ public class GameScreen extends AbstractScreen {
 	private Texture jug;
 	private Jugador player;
 	int posyjugador = 100;
+	private int tamainodisparoaliado=32,veldisparoaliado=10;
+	float enfriamientodisparo= 0.1f;
+	float tiempodisparo=5;
+	ArrayList<Disparo> disparoaliado = new ArrayList<Disparo>();
 	
 	public GameScreen(AirDestructionGame main) {
 		super(main);
@@ -24,7 +30,7 @@ public class GameScreen extends AbstractScreen {
 		batch = new SpriteBatch();
 		texture = new Texture("GameFondo.jpg");
 		jug = new Texture("snorlax.png");
-		player = new Jugador(calcularmitadpantX(),posyjugador,100,100,0,5);
+		player = new Jugador(calcularmitadpantX(),posyjugador,100,100,0,200);
 	}
 	
 	@Override
@@ -34,7 +40,12 @@ public class GameScreen extends AbstractScreen {
 
         batch.begin(); 
         batch.draw(texture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); 
-	batch.draw(jug, player.getX(),posyjugador,player.getAnchura(),player.getAltura());
+        if(disparoaliado!=null&&disparoaliado.size()!=0) {
+        	for (int i = 0; i < disparoaliado.size(); ++i) {
+        		batch.draw(disparoaliado.get(i).getTextura(), disparoaliado.get(i).getX(), disparoaliado.get(i).getY(), disparoaliado.get(i).gettamaino(), disparoaliado.get(i).gettamaino());
+        	}
+        }
+        batch.draw(jug, player.getX(),posyjugador,player.getAnchura(),player.getAltura());
         batch.end(); 
 	entradadatos();
     }
@@ -43,17 +54,18 @@ public class GameScreen extends AbstractScreen {
 		boolean izquierdapulsada = Gdx.input.isKeyPressed(Input.Keys.A)||Gdx.input.isKeyPressed(Input.Keys.LEFT);
 		boolean derechapulsada = Gdx.input.isKeyPressed(Input.Keys.D)||Gdx.input.isKeyPressed(Input.Keys.RIGHT);
 		boolean intentadisparar = Gdx.input.isKeyPressed(Input.Keys.W)||Gdx.input.isKeyPressed(Input.Keys.UP)||Gdx.input.isKeyPressed(Input.Keys.SPACE);
+		float delta=Gdx.graphics.getDeltaTime();
 		if(izquierdapulsada!=derechapulsada) {
 			int nuevaX;
 			if(izquierdapulsada) {
-				nuevaX=(int) (player.getX()-player.getVelocidad());
+				nuevaX=(int) (player.getX()-player.getVelocidad()*delta);
 				if(tocaborde(nuevaX,player)) {
 					player.setX(0);
 				}else {
 					player.setX(nuevaX);
 				}
 			}else if(derechapulsada) {
-				nuevaX=(int) (player.getX()+player.getVelocidad());
+				nuevaX=(int) (player.getX()+player.getVelocidad()*delta);
 				if(tocaborde(nuevaX,player)) {
 					player.setX(Gdx.graphics.getWidth()-player.getAnchura());
 				}else {
@@ -61,9 +73,7 @@ public class GameScreen extends AbstractScreen {
 				}
 			}
 		}
-		if(intentadisparar) {
-			System.out.println("dispara");
-		}
+		gestiondisparosaliado(intentadisparar,delta);
 		
 	}
 	public int calcularmitadpantX() {
@@ -77,6 +87,38 @@ public class GameScreen extends AbstractScreen {
 		}else {
 			return false;
 		}
+	}
+	public void gestiondisparosaliado(boolean intentadisparar,float delta) {
+		if(intentadisparar) {
+			if(tiempodisparo>=enfriamientodisparo) {
+				disparaaliado();
+				tiempodisparo=0;
+			}else {
+				tiempodisparo+=delta;
+			}
+		}
+		if(disparoaliado!=null&&disparoaliado.size()!=0) {
+			for (int i = 0; i < disparoaliado.size(); ++i) {
+				if(disparoaliado.get(i).getY()-disparoaliado.get(i).gettamaino()>Gdx.graphics.getHeight()) {
+					disparoaliado.get(i).dispose();
+					disparoaliado.remove(i);
+				}else if(disparoaliado.get(i).getVelX()+disparoaliado.get(i).gettamaino()<0||disparoaliado.get(i).getX()-disparoaliado.get(i).getX()>Gdx.graphics.getWidth()) {
+					disparoaliado.get(i).dispose();
+					disparoaliado.remove(i);
+				}else {
+					disparoaliado.get(i).setX(disparoaliado.get(i).getX()+disparoaliado.get(i).getVelX());
+					disparoaliado.get(i).setY(disparoaliado.get(i).getY()+disparoaliado.get(i).getVelY());
+				}
+			}
+		}
+		
+		
+	}
+	public void disparaaliado() {
+		Texture textshoot = new Texture("badlogic.jpg");
+		int veldisparoY = 0;
+		Disparo shoot = new Disparo(player.getX()+player.getAnchura()/2-tamainodisparoaliado/2, player.getY(), tamainodisparoaliado, veldisparoY, veldisparoaliado, textshoot);
+		disparoaliado.add(shoot);
 	}
 
 }
