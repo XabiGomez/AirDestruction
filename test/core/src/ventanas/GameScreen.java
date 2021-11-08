13f65758e250145963ae140519cf1bd51160fb99
inigolens <81768110+inigolens@users.noreply.github.com>
2021-11-08 08:40:50 +0100

    Update GameScreen.java

--- test/core/src/ventanas/GameScreen.java
+++ test/core/src/ventanas/GameScreen.java
@@ -1,6 +1,7 @@
 package ventanas;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
