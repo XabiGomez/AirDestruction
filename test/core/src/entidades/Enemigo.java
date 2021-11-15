e81df5609c703179a03486376347a43dcc2af45a
inigolens <81768110+inigolens@users.noreply.github.com>
2021-11-15 08:38:05 +0100

    Update Enemigo.java
    a

--- test/core/src/entidades/Enemigo.java
+++ test/core/src/entidades/Enemigo.java
@@ -3,12 +3,18 @@ package entidades;
 import com.badlogic.gdx.graphics.Texture;
 
 public class Enemigo extends Entidad{
-
 	int dano;
 	public Enemigo(float x, float y , float altura, float anchura, int vida, int velocidadX, int velocidadY, Texture textura, int dano) {
-		super(y, x, altura, anchura, vida, velocidadX, velocidadY, textura);
+		super(x, y, altura, anchura, vida, velocidadX, velocidadY, textura);
 		this.dano = dano;
 		
+	}
+
+	public int getDano() {
+		return dano;
+	}
+	public void setDano(int dano) {
+		this.dano = dano;
 	}
 	
 	
