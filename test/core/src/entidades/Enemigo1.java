aeacd338e67f8696a4601438777dca1e8fca99d6
inigolens <81768110+inigolens@users.noreply.github.com>
2021-12-08 15:11:05 +0100

    Dispara Enem1

--- test/core/src/entidades/Enemigo1.java
+++ test/core/src/entidades/Enemigo1.java
@@ -1,10 +1,19 @@
 package entidades;
 
+import java.util.ArrayList;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 
 public class Enemigo1 extends Enemigo{
-	public Enemigo1(float x, float y , float altura, float anchura, int vida, int velocidadX, int velocidadY, Texture textura, int dano) {
-		super(x, y, altura, anchura, vida, velocidadX, velocidadY, textura, dano);
+	private Texture texturadisparo;
+	private float enfriamientodisparo, disparot;
+	public Enemigo1(float x, float y , float altura, float anchura, int vida, int velocidadX, int velocidadY, Texture textura, int dano,int tipo,Texture texturadisparo) {
+		super(x, y, altura, anchura, vida, velocidadX, velocidadY, textura, dano, tipo);
+		enfriamientodisparo = 2.0f;
+		disparot = 0.0f;
+		this.texturadisparo = texturadisparo;
+		
 	}
 	int vel  = getVelocidadX();
 	@Override
@@ -12,10 +21,29 @@ public class Enemigo1 extends Enemigo{
 		
 		if(getX()<0) {
 			vel = getVelocidadX();
-		}else if(getX()+getAnchura()>600) {
+		}else if(getX()+getAnchura()>Gdx.graphics.getWidth()) {
 			vel = -getVelocidadX();
 		}
 		setX(getX()+vel);
 	}
-	
+	public boolean intentadisparar(ArrayList<Entidad> disparoenemigo) {
+		disparot += Gdx.graphics.getDeltaTime();
+		if(disparot>=enfriamientodisparo) {
+			disparot=0.0f;
+			int probabilidaddisp = 50;
+			if(Math.random()*101 + 1<=probabilidaddisp) {
+				float x = (float) getX()+(getAnchura()/2);
+				float y = getY();
+				float altura= 25;
+				float anchura= 10;
+				int velyd = -1;
+				disparoenemigo.add(new Disparo(x,y,altura,anchura,0,0,velyd,texturadisparo));
+				return true;
+			}else {
+				return false;
+			}
+		}else {
+			return false;
+		}
+	}
 }
